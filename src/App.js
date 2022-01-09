6a131b8f43412f6073bc20a76e151de373c7fdcc
hyunsu <me_2_day>
2022-01-09 16:57:59 +0900

    3Day Commit

--- src/App.js
+++ src/App.js
@@ -1,45 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
-import Header from "./Day-1/Header";
-import Hello from "./Day-1/hello";
-
-import TestProps2 from "./Day-1/props2";
-import TestProps from "./Day-1/props1";
-import Wrapper from "./Day-1/Wrapper";
-import Counter from "./Day-1/Counter";
-import InputSample from "./Day-2/InputSample";
-import MultiInputSample from "./Day-2/MultiInputSample";
-import UseRefSample from "./Day-2/useRefSample";
-import UserList from "./Day-2/UserList";
+import React, { useRef } from 'react';
+import UserList from './Day-3/UserList2';
 
 function App() {
-    const name = "Hyunsul Kim";
-    const style = {
-        backgroundColor : 'black',
-        color: 'aqua',
-        fontSize :'24',
-        padding :'1rem'
+    const users = [
+        {
+            id: 1,
+            username: 'velopert',
+            email: 'public.velopert@gmail.com'
+        },
+        {
+            id: 2,
+            username: 'tester',
+            email: 'tester@example.com'
+        },
+        {
+            id: 3,
+            username: 'liz',
+            email: 'liz@example.com'
+        }
+    ];
 
-    }
-  return (
-      // <Header/>
-      <Wrapper>
-          {/*이렇게 2가지로 사용가능하고 이름만 넣어줘도 true로 인식*/}
-          {/*<Hello name={"react"} color={"red"} isSpecial={true}/>*/}
-          <Hello name={"react"} color={"red"} isSpecial/>
-          <TestProps name={"react"} color={"blue"}/>
-          <TestProps2 />
-          <TestProps2 name={"react"} color={"blue"}/>
-          <div style={style}>{name}</div>
-          <div className="gray-box"/>
-          <Counter/>
-          <InputSample/>
-          <MultiInputSample/>
-          <UseRefSample/>
-          <UserList/>
-      </Wrapper>
+    const nextId = useRef(4);
+    const onCreate = () => {
+        // 나중에 구현 할 배열에 항목 추가하는 로직
+        // ..
 
-  );
+        nextId.current += 1;
+    };
+    return <UserList testusers={users} />;
 }
 
 export default App;
